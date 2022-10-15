e1968819c87bb32dcc7c706e0df0d209f563fcc5
divesh <Diveshgupta401@gmail.com>
2022-10-15 19:29:00 +0530

    initial commit

---
+++ Dayone/Nameusingcommandline.java
@@ -0,0 +1,9 @@
+package Dayone;
+
+public class Nameusingcommandline {
+
+	public static void main(String[] args) {
+		System.out.println("Divesh" + args[0]);
+	}
+
+}
---
+++ Dayone/demonstratestatic.java
@@ -0,0 +1,16 @@
+package Dayone;
+
+public class demonstratestatic {
+	 static int a;
+	 static int b;
+	 static {
+	    a = 10;
+	    b = 20;
+	 }
+	 public static void main(String args[]) {
+
+	  System.out.println("Value of a = " + a);
+	  System.out.println("Value of b = " + b);
+
+	 	}
+}
